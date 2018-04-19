372a30f179a281a1ef07505e346c2022d843fe14
Naoto Shimazaki <nshimaza@ioxsdk.iox.local>
2018-04-19 23:01:01 +0900

    run at app data dir

--- runtime/start_agent.sh
+++ runtime/start_agent.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 . /data/.env
-/usr/local/bin/agent run $CAF_APP_APPDATA_DIR/agent.cfg
+cd $CAF_APP_APPDATA_DIR
+/usr/local/bin/agent run agent.cfg
 echo "$(date) agent exited." >> $CAF_APP_LOG_DIR/start_agent.log
 while true; do
     sleep 1000
