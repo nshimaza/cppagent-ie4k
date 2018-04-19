2e067c6b7b7172acdb9df6d6d1a6b0cec3b35e1c
Naoto Shimazaki <nshimaza@ioxsdk.iox.local>
2018-04-19 22:23:11 +0900

    fix logging

--- runtime/start_agent.sh
+++ runtime/start_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . /data/.env
 /usr/local/bin/agent run $CAF_PERSISTENT_DIR/agent.cfg
-echo "$(date) agent exited." >> CAF_APP_LOG_DIR/start_agent.log
+echo "$(date) agent exited." >> $CAF_APP_LOG_DIR/start_agent.log
 while true; do
     sleep 1000
 done
